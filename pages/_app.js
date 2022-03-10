47898570ae31f61f8200182e32b490d8a12dddb1
Bogdan <bogdan1850@gmail.com>
2022-03-10 13:38:22 +0100

    manifest logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <title>Bogd.app</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="manifest" href="manifest.webmanifest"></link>
+
+      </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
